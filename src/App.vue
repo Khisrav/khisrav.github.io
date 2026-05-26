f9176bced8ed8c5e036c772fd89b6274c51fb016
Khisrav <kh.khisrav2018@gmail.com>
2026-05-27 00:01:04 +0300

    almost done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import VisitCard from "./components/VisitCard.vue";
+import MiniPlayer from "./components/MiniPlayer.vue";
 </script>
 
 <template>
-  <HelloWorld />
+    <VisitCard />
+    <MiniPlayer />
 </template>
